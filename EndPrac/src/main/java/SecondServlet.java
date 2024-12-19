338c6037b55ecd7fc46bb31cc4afa8a226179fcf
DharmBharodiya <dharmbharodiya@gmail.com>
2024-12-19 08:28:01 +0530

    New Files for End Practical Exams AAdded

--- EndPrac/src/main/java/SecondServlet.java
+++ EndPrac/src/main/java/SecondServlet.java
@@ -6,9 +6,11 @@ import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SecondServlet extends HttpServlet {
 	
@@ -24,6 +26,23 @@ public class SecondServlet extends HttpServlet {
 		ServletContext context = getServletContext();
 		String newEmail = context.getInitParameter("email");
 		out.println("Email with context: " + newEmail);
+		
+		
+		Cookie ck = new Cookie("AUTHUSER", "dharm");
+		resp.addCookie(ck);
+		
+		
+		Cookie ckNew[] = req.getCookies();
+		for(int i=0; i < ckNew.length; i++) {
+			out.println("Cookie: " + ckNew[i].getName() + " : " + ckNew[i].getValue());
+		}
+	
+		Cookie ck2 = new Cookie("AUTHUSER", "");
+		ck2.setMaxAge(0);
+		resp.addCookie(ck2);
+		
+		HttpSession session = req.getSession(false);
+		session.setAttribute("uname", "dharm");
 		
 	}
 	
